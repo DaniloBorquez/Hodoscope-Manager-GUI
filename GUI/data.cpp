ed65aa7e97c3be9c5eb9baa2721c8f179d1e1327
Danilo <danilo.borquez@gmail.com>
2018-08-02 10:56:13 -0400

    Data class fields added: frequency, position, azimuth, polar, distance, date.

--- GUI/data.cpp
+++ GUI/data.cpp
@@ -4,3 +4,63 @@ Data::Data()
 {
 
 }
+
+double Data::getFrequency() const
+{
+    return frequency;
+}
+
+void Data::setFrequency(double value)
+{
+    frequency = value;
+}
+
+double Data::getPosition() const
+{
+    return position;
+}
+
+void Data::setPosition(double value)
+{
+    position = value;
+}
+
+double Data::getAzimuth() const
+{
+    return azimuth;
+}
+
+void Data::setAzimuth(double value)
+{
+    azimuth = value;
+}
+
+double Data::getPolar() const
+{
+    return polar;
+}
+
+void Data::setPolar(double value)
+{
+    polar = value;
+}
+
+double Data::getDistance() const
+{
+    return distance;
+}
+
+void Data::setDistance(double value)
+{
+    distance = value;
+}
+
+double Data::getDate() const
+{
+    return date;
+}
+
+void Data::setDate(double value)
+{
+    date = value;
+}
--- GUI/data.h
+++ GUI/data.h
@@ -5,7 +5,36 @@
 class Data
 {
 public:
+    /**
+     * @brief Data Class constructor.
+     */
     Data();
+
+    double getFrequency() const;
+    void setFrequency(double value);
+
+    double getPosition() const;
+    void setPosition(double value);
+
+    double getAzimuth() const;
+    void setAzimuth(double value);
+
+    double getPolar() const;
+    void setPolar(double value);
+
+    double getDistance() const;
+    void setDistance(double value);
+
+    double getDate() const;
+    void setDate(double value);
+
+private:
+    double frequency;
+    double position;
+    double azimuth;
+    double polar;
+    double distance;
+    double date;
 };
 
 #endif // DATA_H
